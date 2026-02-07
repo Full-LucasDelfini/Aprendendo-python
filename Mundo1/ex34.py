da4b2f0fcdc0c31b6a8293ff48c62aca4008f44d
Full-LucasDelfini <lucasdelfini634@gmail.com>
2026-02-07 20:04:39 -0300

    Update ex34.py

--- Mundo1/ex34.py
+++ Mundo1/ex34.py
@@ -3,3 +3,4 @@ if money <= 1250:
     print(f'Você recebeu um aumento de 15%, logo seu novo sálario será: R$ {money * 1.15:.2f}')
 else:
     print(f'Você recebeu um aumento de 10%, logo seu novo sálario será: R$ {money * 1.10:.2f}')
+
