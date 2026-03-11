fbc545752e3f3374417b75e3730087c9064d05a4
Full-LucasDelfini <lucasdelfini634@gmail.com>
2026-03-11 20:43:09 -0300

    Add files via upload

---
+++ Mundo2/ex53.py
@@ -0,0 +1,13 @@
+frase = str(input('Digite uma frase qualquer: ')).strip().upper()
+palavras = frase.split()
+junto = ''.join(palavras)
+inverso = ''
+for letra in range(len(junto) -1, -1, -1):
+    inverso += junto[letra]
+print(f'O inverso de {junto} é {inverso}.')
+if inverso == junto:
+    print(f'A palavra {frase} é um PALÍNDROMO!')
+else:
+    print(f'A palavra {frase} não é um palíndromo')
+
+
