722035451e12563435dc6f3c5ed584094e4342fa
Lucas Delfini <lucasdelfini634@gmail.com>
2026-05-12 17:44:50 -0300

    Add files via upload
    Descrição: Lê vários números, armazena-os em uma lista e extrai estatísticas como: quantidade de elementos, exibição em ordem decrescente e verificação de existência de um valor específico (número 5).
    
    O que pratica: Métodos de lista (len, sort(reverse=True)), operadores de identidade (in) e laços de interrupção (break).

---
+++ Mundo3/ex81.py
@@ -0,0 +1,20 @@
+lista_numeros = []
+while True:
+    num = int(input('Digite um número: '))
+    lista_numeros.append(num)
+    resp = str(input('Quer continuar? [S/N] ')).strip().upper()[0]
+    while resp not in 'SN':
+        resp = str(input('Quer continuar? [S/N] ')).strip().upper()[0]
+    if resp == 'N':
+        break
+print('-=-='*30)
+print(f'Ao todo foram digitados {len(lista_numeros)} números!')
+lista_numeros.sort(reverse=True)
+print(f'A lista de valores de forma decrescente é {lista_numeros}')
+if 5 in lista_numeros:
+    print('O número 5 pertence a lista de números!')
+else:
+    print('O número 5 não pertence a lista!')
+
+
+
