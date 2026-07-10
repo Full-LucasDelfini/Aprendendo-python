3e2692d68dcb23bdfe1bfe432661d9453c17bd7a
Lucas Delfini <lucasdelfini634@gmail.com>
2026-07-09 21:36:02 -0300

    Add files via upload
    Desafio 97: Um Print Especial
    - Descrição: Criação de uma função de estilização de texto para o terminal.
    - Objetivo: Exibir mensagens com linhas de borda (moldura) que se adaptam automaticamente ao comprimento do texto enviado como argumento.
    - O que usou: Funções (def), passagem de parâmetros, manipulação de strings (f-strings) e operadores aritméticos de multiplicação aplicados a caracteres para geração dinâmica de linhas.

---
+++ Mundo3/ex97.py
@@ -0,0 +1,11 @@
+def escreva(msg):
+    tam = len(msg)
+    print('-' * tam)
+    print(txt)
+    print('-' * tam)
+    print()
+
+
+for t in range(0, 3):
+    txt = input('Digite uma frase: ')
+    escreva(txt)
