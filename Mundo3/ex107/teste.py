874f65f5dbffd786fccb1f408e384b1c57e786ad
Lucas Delfini <lucasdelfini634@gmail.com>
2026-07-24 20:41:23 -0300

    Delete Mundo3/ex107 directory

--- Mundo3/ex107/teste.py
+++
@@ -1,6 +0,0 @@
-from ex107 import moeda
-v = float(input('Digite um valor: '))
-print(f'A metade de {v} é: {moeda.metade(v)}')
-print(f'O dobro de {v} é: {moeda.dobro(v)}')
-print(f'Aumentando 10%, temos {moeda.aumentar(v,10)}')
-print(f'Diminuindo 15% temos {moeda.diminuir(v, 15)}')
