fe4c9faa4dc01a473c743424c7456ada089b7b29
Lucas Delfini <lucasdelfini634@gmail.com>
2026-07-24 20:41:53 -0300

    Add files via upload

---
+++ Mundo3/ex107.py/moeda.py
@@ -0,0 +1,11 @@
+def dobro (num):
+    return num * 2
+
+def metade (num):
+    return num / 2
+
+def aumentar (num, porcentagem):
+    return num + (num * porcentagem/100)
+
+def diminuir (num, porcentagem):
+    return num - (num * porcentagem/100)
