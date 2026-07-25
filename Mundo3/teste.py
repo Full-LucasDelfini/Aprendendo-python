87085b3ec5cd469a03cc0e03050a35903126287e
Lucas Delfini <lucasdelfini634@gmail.com>
2026-07-25 17:56:05 -0300

    Delete Mundo3/teste.py

--- Mundo3/teste.py
+++
@@ -1,6 +0,0 @@
-from ex108 import moeda
-v = float(input('Digite um valor: '))
-print(f'A metade de {moeda.moeda(v)} é: {moeda.moeda(moeda.metade(v))}')
-print(f'O dobro de {moeda.moeda(v)} é: {moeda.moeda(moeda.dobro(v))}')
-print(f'Aumentando 10% temos {moeda.moeda(moeda.aumentar(v, 10))}')
-print(f'Diminuindo 15% temos {moeda.moeda(moeda.diminuir(v, 15))}')
