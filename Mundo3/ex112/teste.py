05625ee2afca8f82c7fdc7c06703e4edd55adc87
Lucas Delfini <lucasdelfini634@gmail.com>
2026-07-29 20:14:59 -0300

    Add files via upload

---
+++ Mundo3/ex112/teste.py
@@ -0,0 +1,4 @@
+from ex112.utilidadescev import moeda
+from ex112.utilidadescev import dado
+p = dado.leiaDinheiro('Digite um preço: R$ ')
+moeda.resumo(p,15,10)
---
+++ Mundo3/ex112/utilidadescev/moeda/__init__.py
@@ -0,0 +1,28 @@
+def dobro(preço, formato=False):
+    res = preço * 2
+    return res if formato is False else (moeda(res))
+
+def metade(preço,formato=False):
+    res = preço / 2
+    return res if formato is False else (moeda(res))
+
+def aumentar(preço=0, porcentagem=0, formato=False):
+    res = preço + (preço * porcentagem/100)
+    return res if formato is False else (moeda(res))
+
+def diminuir(preço=0, porcentagem=0, formato=False):
+    res = preço - (preço * porcentagem / 100)
+    return res if not formato else (moeda(res))
+
+def moeda(preço=0, formate='R$'):
+    return f'{formate}{preço:.2f}'.replace('.',',')
+
+def resumo(preço=0, taxaau=10, taxare=5):
+    print('='*30)
+    print('RESUMO DO VALOR'.center(30))
+    print('='*30)
+    print(f'Preço analisado: \t{moeda(preço)}')
+    print(f'Dobro do preço: \t{dobro(preço, True)}')
+    print(f'Metade do preço: \t{metade(preço, True)}')
+    print(f'{taxaau}% de aumento: \t{aumentar(preço, taxaau, True)}')
+    print(f'{taxare}% de redução: \t{diminuir(preço, taxare, True)}')
